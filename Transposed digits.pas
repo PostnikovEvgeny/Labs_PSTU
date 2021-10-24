13f7f925ef1ed2c7f0c59125bcb92c6b2e4bc128
Постников Евгений Рис-21-1б <92161003+PostnikovEvgeny@users.noreply.github.com>
2021-10-24 15:11:44 +0500

    Программы: "Угадай число a"," Перестановка цифр"

---
+++ Transposed digits.pas
@@ -0,0 +1,120 @@
+﻿var
+	 x, a, b, c, t, f:integer;
+	
+	begin
+   a:=0;b:=0;c:=0;t:=0;
+	f :=0;
+
+	writeln( 'Введите трёхзначное число');
+readln (x);
+	while ((x < 100) or (x > 999)) do begin
+	
+		writeln('Введённое число не трёхзначное, повторите ввод');
+	readln( x);
+	end;
+	a:= x mod 10;
+	x:= x div 10;
+	b:= x mod 10;
+	c:= x div 10;
+
+
+	while (f = 0) do begin
+		
+		if ((a = b) or (b = c) or (a = c)) then begin
+			writeln('есть одинаковые цифры,введите новое число');
+			readln(x);
+			while ((x < 100) or (x > 999)) do begin
+	
+		writeln('Введённое число не трёхзначное, повторите ввод');
+	readln( x);end;
+	
+	a:= x mod 10;
+	x:= x div 10;
+	b:= x mod 10;
+	c:= x div 10;
+end
+		else  f:= 1; 
+end;
+		writeln('все возможные  последовательности чисел:');
+		writeln( a, b, c);
+		t:= c;
+		c:= b;
+		b:= t;
+	writeln( a, b, c);
+		t:= a;
+		a:= b;
+		b:= t;
+	writeln( a, b, c);
+		t:= a;
+		a:= c;
+		c:= t;
+	writeln( a, b, c);
+		t:= c;
+		c:= b;
+		b:= t;
+	writeln( a, b, c);
+		t:= a;
+		a:= b;
+		b:= t;
+	writeln( a, b, c);
+		t:= c;
+		c:= b;
+		b:= t;
+	writeln( a, b, c);
+
+
+
+		if ((a > b) and (a > c)) then begin
+		
+			write('Наибольшее из этих чисел:', a);
+			if (b > c) then
+			
+				writeln (b,c)
+			
+			else
+			
+				writeln (c,b);
+			
+		end
+		else
+		begin
+			if ((b > a) and (b > c)) then begin
+			
+				write('Наибольшее из этих чисел:',b);
+				if (a > c) then
+				
+					writeln (a,c)
+				
+				else
+				
+					writeln (c,a);
+				
+			end
+			else
+			
+				if ((c > b) and (c > a))then begin
+				
+					write('Наибольшее из этих чисел:',c);
+					if (b > a) then
+					
+					writeln (b,a)
+					
+					else
+					
+						writeln (a,b);
+					
+				end
+				else
+				
+				writeln (a,b);
+				
+			
+		end;
+		end.
+
+
+	
+
+
+			
+		
---
+++ guess the number a.pas
@@ -0,0 +1,13 @@
+﻿var a,k,p,i: integer;
+begin
+a := random(1,100);
+writeln('угадайте число: a');
+while (k<>7) and (p<1) do begin 
+writeln('введите число');
+readln(i);
+if (i>a) then begin writeln('введённое число больше, чем a');k+=1;end else
+If (i<a) then begin writeln('введённое число меньше, чем a'); k+=1; end else
+If (i=a) then begin writeln('вы угадали число! a=',a);p+=1;end;
+end;
+If k=7 then  writeln('вы не угадали число! a=',a); 
+end.
